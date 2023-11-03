19d31d66d03d31d2eccb498375698be6a196e865
Peterr134 <peterr134@nycstudents.net>
2023-11-03 08:58:03 -0400

    Finished!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
 public class Main {
+    static boolean[] doorValues = new boolean[3];
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for(int i = 0; i<3; i++){
+            doorValues[i] = false;
+        }
+        int randomNum = (int)(Math.random()*3);
+        doorValues[randomNum] = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Monty Hall simulator!");
+        System.out.println("There are 3 doors. One has a car, and the other two have goats");
+        System.out.println("Which door will you choose? 1, 2, or 3?");
+        int playerChoice = scanner.nextInt()-1;
+        Monty monty = new Monty(playerChoice, randomNum);
+        System.out.println("There is a goat behind door " + monty.goatReveal() + "!");
+        System.out.println("Do you want to switch your pick?");
+        monty.updateChoice();
+        int winDoor;
+        if(doorValues[0]){
+            System.out.println("The car is behind door 1!");
+            winDoor = 0;
+        }else if(doorValues[1]){
+            System.out.println("The car is behind door 2!");
+            winDoor = 1;
+        }else{
+            System.out.println("The car is behind door 3!");
+            winDoor = 2;
+        }
+        if(monty.getNewPlayerChoice() == winDoor){
+            System.out.println("You win the car!");
+        }else{
+            System.out.println("You win a goat!");
+        }
+
     }
 }
--- src/Monty.java
+++ src/Monty.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Monty {
+import java.util.Scanner;
+public class Monty {
+    private int playerChoice;
+    private int carDoor;
+    private int newPlayerChoice;
+    private int revealedDoor;
+    public Monty(int playerChoice, int carDoor){
+        this.playerChoice = playerChoice;
+        this.carDoor = carDoor;
+    }
+    public int goatReveal() {
+        int goatReveal;
+        while (true) {
+            goatReveal = (int) (Math.random() * 3);
+            if (!(goatReveal == carDoor || goatReveal == (playerChoice))) {
+                revealedDoor = goatReveal;
+                return(goatReveal+1);
+            }
+        }
+    }
+    public void updateChoice() {
+        Scanner scanner = new Scanner(System.in);
+        String playerSwitch = scanner.nextLine();
+        if (playerSwitch.equalsIgnoreCase("yes")) {
+            if (revealedDoor == 1) {
+                if (playerChoice == 0) {
+                    newPlayerChoice = 2;
+                } else {
+                    newPlayerChoice = 0;
+                }
+            } else if (revealedDoor == 0) {
+                if (playerChoice == 1) {
+                    newPlayerChoice = 2;
+                } else {
+                    newPlayerChoice = 1;
+                }
+            } else {
+                if (playerChoice == 0) {
+                    newPlayerChoice = 1;
+                } else {
+                    newPlayerChoice = 0;
+                }
+            }
+        } else {
+            newPlayerChoice = playerChoice;
+        }
+        if (newPlayerChoice != playerChoice) {
+            System.out.println("Guess switched from " + (playerChoice + 1) + " to " + (newPlayerChoice + 1));
+        }else{
+            System.out.println("Guess not switched");
+        }
+    }
+
+    public int getNewPlayerChoice() {
+        return newPlayerChoice;
+    }
 }
